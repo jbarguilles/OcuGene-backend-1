17115dc9961bfc715f12e41502a291aa17c4dde7
msrosales <msrosales@up.edu.ph>
2024-09-13 13:55:07 +0800

    Sprinboot Initialization

---
+++ ocugene/src/main/java/com/ocugene/OcugeneApplication.java
@@ -0,0 +1,13 @@
+package com.ocugene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcugeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcugeneApplication.class, args);
+	}
+
+}
---
+++ ocugene/src/test/java/com/ocugene/OcugeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocugene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcugeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
