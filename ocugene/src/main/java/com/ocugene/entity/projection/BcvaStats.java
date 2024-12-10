b3d5c71ef854e5c9e6b9f1c582039e5b51b8c851
jbarguilles <jbarguilles@up.edu.ph>
2024-12-10 13:39:42 +0800

    added get bcva stats

---
+++ ocugene/src/main/java/com/ocugene/entity/projection/BcvaStats.java
@@ -0,0 +1,9 @@
+package com.ocugene.entity.projection;
+
+public interface BcvaStats {
+    Integer getCount2020();
+    Integer getCount2040();
+    Integer getCount2060();
+    Integer getCount2080();
+    Integer getCount20100();
+}
